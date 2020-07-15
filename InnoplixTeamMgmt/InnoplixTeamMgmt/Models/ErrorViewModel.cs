fdde5a7ce17910b9d8dab1d5969061641b4ba666
Manoj Kumar <manoj.kumar@modmed.com>
2020-07-15 11:25:13 +0530

    created project

---
+++ InnoplixTeamMgmt/InnoplixTeamMgmt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InnoplixTeamMgmt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
